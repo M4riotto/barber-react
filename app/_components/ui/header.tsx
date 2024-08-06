05965543cceb9a8182a25c0f9b866cf020d48b86
Vitor Mariotto <vitormariotto03@gmail.com>
2024-08-06 15:53:19 -0300

    feat: add header compnent and banner

---
+++ app/_components/ui/header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import {Card, CardContent} from "./card"
+import { Button } from "./button";
+import { MenuIcon } from "lucide-react";
+const Header = () => {
+    return ( 
+        <Card>
+            <CardContent className="p-5 flex flex-row items-center justify-between">
+                <Image alt="" src="/logo.png" width={120} height={18} />
+                <Button size="icon" variant="outline">
+                    <MenuIcon />
+                </Button>
+            </CardContent>
+        </Card>
+     );
+}
+ 
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Víto!</h2>
+        <p>Segunda-Feira, 05 de Agosto</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6 ">
+          <Image alt="" src="/banner-01.png" fill className="object-cover rounded-xl"/>
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Home

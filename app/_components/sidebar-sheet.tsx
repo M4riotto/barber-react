a2ceeeb4b8f0be44b903e57cae6c99bd88bbdcbd
Vitor Mariotto <vitormariotto03@gmail.com>
2024-08-09 18:16:28 -0300

    fix: hide signOut

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -86,12 +86,14 @@ const SidebarSheet = () => {
             </div>
 
 
-            <div className="py-5 flex flex-col gap-2">
-                <Button variant="ghost" className="justify-start gap-2" onClick={handleLogoutClick}>
-                    <LogOutIcon size={18} />
-                    Sair da conta
-                </Button>
-            </div>
+          {data?.user && (
+              <div className="py-5 flex flex-col gap-2">
+              <Button variant="ghost" className="justify-start gap-2" onClick={handleLogoutClick}>
+                  <LogOutIcon size={18} />
+                  Sair da conta
+              </Button>
+          </div>
+          )}
 
         </SheetContent>
     );
